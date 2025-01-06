f89b81f4d4a0265201615806ab79aa166844fa3e
Emerson <70976306+emerdesenv@users.noreply.github.com>
2025-01-06 11:54:29 -0300

    build: Mudança estrutura

---
+++ src/cypress/e2e/api/api_login.cy.js
@@ -0,0 +1,44 @@
+describe.only('API - Teste funcional de Login', () => {
+    it('Deve realizar o login com sucesso', () => {
+        //npx serverest@latest
+        //Cypress.env('urlApiLocal')
+
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/login',
+            body: {
+                "email": "fulano@qa.com",
+                "password": "teste"
+            }
+        }).then((response)=>{
+            expect(response.status).to.equal(200)
+            expect(response.body.message).to.equal('Login realizado com sucesso')
+        })
+    });
+
+    it('Deve validar senha incorreta', () => {
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3000/login',
+            body: {
+                "email": "fulano@qa.com",
+                "password": "testinho"
+            },
+            failOnStatusCode: false
+        }).then((response)=>{
+            expect(response.status).to.equal(401)
+            expect(response.body.message).to.equal('Email e/ou senha inválidos')
+        })
+    });
+    
+    it('Pega um usuário existente', () => {
+        cy.request({
+            method: 'GET',
+            url: 'http://localhost:3000/usuarios/0uxuPY0cbmQhpEz1'
+        }).then((response)=>{
+            expect(response.status).to.equal(200)
+
+            cy.log('Nome: '+response.body.nome);
+        })
+    });
+});
