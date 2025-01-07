2426164d19e921db5e8f5b3e2e8eeafb1824e0ab
Emerson <70976306+emerdesenv@users.noreply.github.com>
2025-01-07 15:10:42 -0300

    refactor: Teste [login]

--- cypress/e2e/login/gui_login.cy.js
+++ cypress/e2e/login/gui_login.cy.js
@@ -2,6 +2,8 @@ describe('Testes de Login!', { tags: ['login'] }, () => {
     it.only('Deve realizar o login com sucesso.', () => {
        cy.visit(Cypress.env('urlSite'));
 
+       //teste
+
        cy.get('[data-test="username"]').type('standard_user');
        cy.get('[data-test="password"]').type('secret_sauce');
        cy.get('[data-test="login-button"]').click();
